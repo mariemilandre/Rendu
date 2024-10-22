7655aaf92acf1795f0c43f6866e471217fd860e7
mariemilandre <myemail>
2024-10-22 15:01:37 +0200

    change name

---
+++ Développement langage python/functions.py
@@ -0,0 +1,20 @@
+def several_zeros():
+   return '000000'
+
+def several_zeros_custom(nb_zeros):
+   malist = []
+   for i in range(nb_zeros):
+        malist.append(0)
+   return malist
+
+def matrix(rows, cols):
+    matrix[
+        [1, 5],
+        [2, 6],
+        [2, 8],
+        ]
+
+    pass
+
+if __name__ == '__main__':
+    print(several_zeros_custom(2))
